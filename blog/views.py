cc1b2c881fa537c1d3123ac1ec33b5c73df3b715
stepkos <stepkos@wp.pl>
2022-08-17 23:32:28 +0200

    Database with first model

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Stepkos',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
 
 def home(request):
-    context = {
-        'posts': posts
-    }
-    return render(request, 'blog/home.html', context)
+    return render(request, 'blog/home.html', {
+        'posts': Post.objects.all()
+    })
+
 
 def about(request):
     return render(request, 'blog/about.html', {
