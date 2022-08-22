3a8283ca1ea914a5beace7ff40c97177c083bf69
stepkos <stepkos@wp.pl>
2022-08-22 23:52:23 +0200

    Class based views CRUD

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 
 
@@ -6,6 +14,54 @@ def home(request):
     return render(request, 'blog/home.html', {
         'posts': Post.objects.all()
     })
+
+
+class PostListView(ListView):
+    
+    model = Post
+
+    # Default template path patern
+    # <app>/<model>_<viewtype>.html
+    template_name = 'blog/home.html'
+
+    # Name of objects in template
+    context_object_name = 'posts' 
+
+    # Order records "-"" mean desc
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # def form_valid(self, form):
+    #     form.instance.author = self.request.user
+    #     return super().form_valid(form)
+
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        return self.request.user == self.get_object().author
 
 
 def about(request):
